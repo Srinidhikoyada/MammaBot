56d027cd84964ccb3d5b97374b38eaf69d3b0a9b
Srinidhikoyada <srinidhikoyada@gmail.com>
2024-07-13 23:26:14 +0530

    Add files via upload
    Introducing MamaBot, your trusted companion throughout your pregnancy journey. Designed with care and precision, MamaBot provides expectant mothers with personalized support, valuable insights, and up-to-date information about their pregnancy. From answering questions about symptoms to offering tips on nutrition and wellness, MamaBot is here to guide you every step of the way. With its friendly interface and soothing pastel design, MamaBot ensures a comforting and engaging experience. Let MamaBot be your go-to resource, empowering you to enjoy a healthier and happier pregnancy.

---
+++ Batch no 1/Batch no 1/MamaBot A System Based on ML and NLP for Supporting Women and Families during Pregnancy/Chatbot/MyChatBot/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [path("index.html", views.MyChatBot, name="MyChatBot"),
+	       path("User.html", views.User, name="User"),
+	       path("index.html", views.Logout, name="Logout"),
+	       path("Register.html", views.Register, name="Register"),
+               path("facts.html", views.facts, name="facts"),
+               path("schedules.html", views.schedules, name="schedules"),
+	       path("test.html", views.test, name="test"),
+	       path("UserLogin", views.UserLogin, name="UserLogin"),
+	       path("Signup", views.Signup, name="Signup"),
+	       path("ChatData", views.ChatData, name="ChatData"),
+]
